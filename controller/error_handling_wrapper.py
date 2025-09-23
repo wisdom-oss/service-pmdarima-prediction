a1262814f6c50fe504c1c65acb122d2d2805776a
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-09-23 11:27:04 +0200

    chore: implementing new error type on `check_for_empty_json_fields`

--- controller/error_handling_wrapper.py
+++ controller/error_handling_wrapper.py
@@ -1,16 +1,22 @@
 from functools import wraps
 from flask import jsonify, request
+from interfaces import ServiceError
 
 def check_for_empty_json_fields(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         data = request.json
         if not data:
-            return jsonify({"error": "Missing JSON body"}), 400
+            raise ServiceError("", 400, "Bad Request", "JSON Request Body Missing", [])
 
         # Check if any value is None
-        if any(value is None for value in data.values()):
-            return jsonify({"error": "Null or undefined value found in request"}), 400
+        exceptions: list[Exception] = []
+        for key, value in data.items():
+            if value == None:
+                exceptions.append(Exception(f"The field '{key}' contained 'null' as value"))
+
+        if len(exceptions) != 0:
+            raise ServiceError("", 400, "Bad Request", f"At least one field in the request body contains a 'null' value", exceptions)
 
         return f(*args, **kwargs)
     return decorated_function
