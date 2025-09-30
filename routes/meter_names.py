ff97b6aea40b32ba887b2b19bbbb37694f91f3d6
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-09-30 11:46:07 +0200

    chore: reimplementing `meterNames` route

---
+++ routes/meter_names.py
@@ -0,0 +1,26 @@
+from __main__ import app
+from database.db_connector import create_connection
+from classes import SmartMeter
+from typing import Any
+from flask_pydantic import validate
+
+
+@app.get("/meter-names")
+@validate(response_many=True)
+def get_meter_names() -> list[SmartMeter]:
+    """
+    `GET /meterNames`
+
+    This method generates meter names for all Smart Meters
+    """
+
+    with create_connection() as db:
+        with db.cursor() as cursor:
+            cursor.execute(
+                "SELECT DISTINCT name FROM timeseries.water_demand_prediction"
+            )
+            result = cursor.fetchall()
+            return [
+                SmartMeter(id=i[0], name=" ".join(i[0].split("-")).title())
+                for i in result
+            ]
