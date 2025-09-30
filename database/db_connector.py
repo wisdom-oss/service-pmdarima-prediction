bb5ddf5276b3b530bda3c7c68b97c0af1eb973b3
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-09-30 12:25:24 +0200

    chore: implementing singleton behaviour for db connection

--- database/db_connector.py
+++ database/db_connector.py
@@ -6,6 +6,9 @@ import logging
 
 from __main__ import config
 
+from psycopg.rows import TupleRow
+
+__connection: Connection[TupleRow] | None = None
 
 def create_connection() -> Connection:
     """
@@ -13,15 +16,16 @@ def create_connection() -> Connection:
 
     :return: connection object
     """
+    global __connection
 
-    connection = psycopg.connect(
-        dbname=config.database_schema_name,
-        user=config.database_user,
-        password=config.database_password,
-        host=config.database_host,
-        port=config.database_port,
-    )
-    if connection:
-        return connection
-    else:
-        raise Exception("Database connection failed")
+    if __connection is None or __connection.closed:
+
+        __connection = psycopg.connect(
+            dbname=config.database_schema_name,
+            user=config.database_user,
+            password=config.database_password,
+            host=config.database_host,
+            port=config.database_port,
+        )
+
+    return __connection
