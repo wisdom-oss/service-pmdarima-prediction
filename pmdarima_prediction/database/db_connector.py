1b6adb8fb5a967e8eb64be3dd4c5f8fac2dc9cb7
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-11-26 13:15:00 +0100

    chore: adding function to return engine and connect it if one isn't available

--- pmdarima_prediction/database/db_connector.py
+++ pmdarima_prediction/database/db_connector.py
@@ -5,6 +5,22 @@ from .. import config
 __connection: Engine | None = None
 
 
+def get_engine() -> Engine:
+    global __connection
+
+    if __connection is None:
+        __url = URL.create(
+            drivername="postgresql",
+            username=config.db_user,
+            password=config.db_password.get_secret_value(),
+            host=config.db_host,
+            database=config.db_name,
+        )
+        __connection = create_engine(__url)
+
+    return __connection
+
+
 def create_connection() -> Connection:
     """
     create a database connection to a Postgres database
