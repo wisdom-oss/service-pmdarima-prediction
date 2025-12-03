d135948ec16522f08dde0a9b093b6acc7b2c301f
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-12-03 10:29:12 +0100

    chore: cleanup imports for application

--- pmdarima_prediction/__init__.py
+++ pmdarima_prediction/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from werkzeug.exceptions import HTTPException
 
+from .exceptions import ServiceException
 from .settings import Settings
 
-config = Settings()
+config = Settings()  # pyright:ignore reportCallIssue
 
 app = Flask(__name__)
 
-from .routes import *
+from .routes import *  # noqa: E402, F403
